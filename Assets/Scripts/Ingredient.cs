00511b7df50cddcec8c70f179599e317908f8042
David Thomson <dwlt@dwlt.net>
2017-01-21 07:41:50 +0000

    Add Ingredient and Utensil scripts

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using VRTK;
+
+public class Ingredient : VRTK_InteractableObject
+{
+	private VRTK_ControllerActions controllerActions;
+
+	public enum IngredientType
+	{
+		Gardened, Hunted, Mined
+	}
+
+	[Header("Custom Options", order = 1)]
+
+	[Tooltip("The type of ingredient")]
+	public IngredientType ingredientType = IngredientType.Gardened;
+
+	public override void Grabbed(GameObject currentGrabbingObject)
+	{
+		controllerActions = currentGrabbingObject.GetComponent<VRTK_ControllerActions>();
+		Utensil utensil = currentGrabbingObject.GetComponent<Utensil>();
+
+		if (utensil) 
+		{
+			if ((ingredientType == IngredientType.Mined && Utensil.UtensilType.Pickaxe == utensil.utensilType) || 
+				(ingredientType == IngredientType.Hunted && Utensil.UtensilType.Knife == utensil.utensilType) || 
+				(ingredientType == IngredientType.Gardened && Utensil.UtensilType.Shears == utensil.utensilType))
+			{
+				if (controllerActions) 
+				{ 
+					controllerActions.TriggerHapticPulse(0.5f);
+				}
+				base.Grabbed(currentGrabbingObject);
+			}
+			else
+			{
+				if (controllerActions) 
+				{ 
+					controllerActions.TriggerHapticPulse(0.75f, 0.3f, 0.01f);
+				}
+			}
+		}
+	}
+}
+
---
+++ Assets/Scripts/Utensil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class Utensil : MonoBehaviour
+{
+	public enum UtensilType 
+	{
+		Shears, Knife, Pickaxe
+	}
+
+	[Tooltip("The type of utensil")]
+	public UtensilType utensilType = UtensilType.Shears;
+}
+
