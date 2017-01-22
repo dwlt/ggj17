6bd278944b9226cd63f858ff1979f3afde3d147a
Ross Anderson <rossanderson1993@gmail.com>
2017-01-22 11:25:56 +0000

    Launching prefab

---
+++ Assets/ObjectConfetti.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectConfetti : MonoBehaviour {
+    public Rigidbody toLaunch;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void launchObjects()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Rigidbody instance = Instantiate(toLaunch, transform);
+            GetRandomUpwardsVelocity(instance);
+        }
+    }
+
+    private void GetRandomUpwardsVelocity(Rigidbody toLaunch)
+    {
+        toLaunch.velocity = new Vector3(Random.Range(0, 3), Random.Range(0, 10), Random.Range(0, 3));
+    }
+}
